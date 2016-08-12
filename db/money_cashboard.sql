1e735db99cded242e6e854af8b3572e8d17a3273
Robbie Campbell <rcampbell182@gmail.com>
2016-08-12 16:41:18 +0100

    set up database

---
+++ db/money_cashboard.sql
@@ -0,0 +1,13 @@
+DROP TABLE transactions;
+DROP TABLE merchants;
+
+CREATE TABLE merchants (
+  id serial4 primary key,
+  name VARCHAR(255)
+);
+
+CREATE TABLE transactions (
+  id serial4 primary key,
+  value int4,
+  tag VARCHAR(255)
+);
