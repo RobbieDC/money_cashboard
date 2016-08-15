4998e3f254aea93ae4393f3f55125e0e77406452
Robbie Campbell <rcampbell182@gmail.com>
2016-08-15 11:05:08 +0100

    finished form page for entering transaction details

--- controllers/transactions_controller.rb
+++ controllers/transactions_controller.rb
@@ -7,38 +7,9 @@ get "/transactions" do
   erb( :"transactions/new" )
 end
 
-# #index
-# get '/books' do
-#   @books = Book.all
-#   erb(:'books/index')
-# end
-
-# #new
-# get '/books/new' do
-#   erb(:'books/new')
-# end
-
-# #show
-# get '/books/:id' do
-#   @book = Book.find(params['id'])
-#   erb(:'books/show')
-# end
-
-# #edit
-# get '/books/:id/edit' do
-# end
-
-# #create
-# post '/books' do
-#   @book = Book.new(params)
-#   @book.save
-#   redirect to( "books" )
-# end
-
-# #update
-# post '/books/:id' do
-# end
-
-# #delete
-# delete '/books' do
-# end
+# create
+post "/transactions" do
+  @transaction = Transaction.new( params )
+  @transaction.save()
+  erb( :"transactions/create" )
+end
