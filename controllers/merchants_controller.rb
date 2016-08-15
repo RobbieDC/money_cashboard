4447ed1a886a547c3e35fa1160e14c7986ef2dfb
Robbie Campbell <rcampbell182@gmail.com>
2016-08-15 12:51:00 +0100

    added route to allow user to register a merchant

--- controllers/merchants_controller.rb
+++ controllers/merchants_controller.rb
@@ -2,10 +2,22 @@ require_relative( "../models/merchant" )
 
 # 7 RESTful routes [ new, create, index, show, edit, update, destroy ]
 
+# new
+get "/merchants/new" do
+  erb( :"merchants/new" )
+end
+
 # index
 get "/merchants" do
   @merchants = Merchant.all()
   erb( :"merchants/index" )
+end
+
+# create
+post "/merchants" do
+  @merchant = Merchant.new( params )
+  @merchant.save()
+  erb( :"merchants/create" )
 end
 
 # show
--- controllers/transactions_controller.rb
+++ controllers/transactions_controller.rb
@@ -3,7 +3,7 @@ require_relative( "../models/transaction" )
 # 7 RESTful routes [ new, create, index, show, edit, update, destroy ]
 
 # new
-get "/transactions" do
+get "/transactions/new" do
   erb( :"transactions/new" )
 end
 
@@ -11,5 +11,5 @@ end
 post "/transactions" do
   @transaction = Transaction.new( params )
   @transaction.save()
-  erb( :"transactions/create" )
+  erb(:"transactions/create")
 end
