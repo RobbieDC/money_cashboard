93a1a8a1e9e76470ffecaeff3d6d18955a30243f
Robbie Campbell <rcampbell182@gmail.com>
2016-08-15 14:26:47 +0100

    added tag table to database

--- db/money_cashboard.sql
+++ db/money_cashboard.sql
@@ -2,9 +2,12 @@ DROP TABLE transactions;
 DROP TABLE merchants;
 DROP TABLE tags;
 
-
-
 CREATE TABLE merchants (
+  id serial4 primary key,
+  name VARCHAR(255)
+);
+
+CREATE TABLE tags (
   id serial4 primary key,
   name VARCHAR(255)
 );
@@ -13,5 +16,6 @@ CREATE TABLE transactions (
   id serial4 primary key,
   value int4,
   tag VARCHAR(255),
-  merchant_id int4 references merchants(id) ON DELETE CASCADE
+  merchant_id int4 references merchants(id) ON DELETE CASCADE,
+  tag_id int4 references tags(id) ON DELETE CASCADE
 );
