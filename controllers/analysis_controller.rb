be25d958969305b70fe17981276c2ae0595ac724
Robbie Campbell <rcampbell182@gmail.com>
2016-08-17 14:17:36 +0100

    created preliminary table for analysis view

--- controllers/analysis_controller.rb
+++ controllers/analysis_controller.rb
@@ -2,6 +2,13 @@ require_relative( "../models/analysis" )
 
 # index
 get "/analysis" do
-  @transactions_per_week = Analysis.group_transactions_by_week()
+  options = {
+    "merchants" => Merchant.all(),
+    "tags" => Tag.all(),
+    "transactions" => Transaction.all()
+  }
+  @analysis = Analysis.new( options )
+  @transactions_per_week = @analysis.group_transactions_by_week()
+  @spent_per_week = @analysis.total_spent_per_week()
   erb( :"analysis/index" )
 end
