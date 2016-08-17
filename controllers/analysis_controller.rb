c6357ce16defbc506b4c7b4467da7da0d185932c
Robbie Campbell <rcampbell182@gmail.com>
2016-08-17 11:58:04 +0100

    fixed bugs in analysis route

--- controllers/analysis_controller.rb
+++ controllers/analysis_controller.rb
@@ -1,13 +1,7 @@
-require_relative(../models/analysis)
+require_relative( "../models/analysis" )
 
 # index
 get "/analysis" do
   @transactions_per_week = Analysis.group_transactions_by_week()
   erb( :"analysis/index" )
 end
-
-# # index
-# get "/merchants" do
-#   @merchants = Merchant.all()
-#   erb( :"merchants/index" )
-# end
--- main.rb
+++ main.rb
@@ -5,6 +5,7 @@ require( "pry-byebug" )
 require_relative( "controllers/merchants_controller" )
 require_relative( "controllers/transactions_controller" )
 require_relative( "controllers/tags_controller" )
+require_relative( "controllers/analysis_controller" )
 
 get "/" do
   erb( :home )
